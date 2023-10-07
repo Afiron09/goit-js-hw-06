2209bcd804a910471e23454cde77f1f184d27c29
Afiron09 <comixgod@gmail.com>
2023-10-07 22:32:43 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createLi = ingredients.map(ingredient => { 
+  const elemLi = document.createElement('li');
+  elemLi.textContent = ingredient
+  elemLi.classList = 'item'
+  return elemLi
+})
+const list = document.querySelector('#ingredients')
+list.append(...createLi)
